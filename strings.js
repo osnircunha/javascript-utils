c8a6599c943cc5aea2cf9cd01932b8284a70ddb7
Osnir F CUNHA <osnircunha@gmail.com>
2015-10-28 23:41:26 -0200

    Update strings.js

--- strings.js
+++ strings.js
@@ -1,16 +1,17 @@
 // Useful methods to handle strings
 
-
-/*
- * Replaces all {n} tokens from a string with a array value.
- * 
- * Params
- * 	stringArray: An array with text to replace.
- * 	text: The text with the {n} tokens to be replaced. 
- * 
- * */
-function replaceToken(stringArray, text){
-	return text.replace(/\{(\d+)\}/g, function(match, contents, offset, totalString) {
-	    return strings[contents];
-	});
+function Utils(){
+	/*
+	 * Replaces all {n} tokens from a string with a array value.
+	 * 
+	 * Params
+	 * 	stringArray: An array with text to replace.
+	 * 	text: The text with the {n} tokens to be replaced. 
+	 * 
+	 * */
+	function replaceToken(stringArray, text){
+		return text.replace(/\{(\d+)\}/g, function(match, contents, offset, stringArray) {
+		    return strings[contents];
+		});
+	}
 }
